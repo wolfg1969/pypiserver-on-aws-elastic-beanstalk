ba0903928a3b697dbe862083c17d8ae92e9b1695
wolfg1969 <wolfg1969@gmail.com>
2013-07-16 20:16:33 +0800

    initial import

---
+++ application.py
@@ -0,0 +1,18 @@
+import errno
+import os
+import os.path as path
+
+import pypiserver
+
+CWD = os.getcwd()
+PACKAGES = path.join(CWD, 'data')
+HTPASSWD = path.join(CWD, 'htpasswd')
+
+try:
+    os.makedirs(PACKAGES)
+except OSError as e:
+    if e.errno != errno.EEXIST:
+        raise
+
+application = pypiserver.app(root=PACKAGES, password_file=HTPASSWD)
+
