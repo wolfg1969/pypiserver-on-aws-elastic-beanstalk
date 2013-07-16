98dbf2b2e42034493aa922b1d274c25c29f6efb2
wolfg1969 <wolfg1969@gmail.com>
2013-07-16 22:38:57 +0800

    read user:password pair from PARAM1 env var

--- application.py
+++ application.py
@@ -3,19 +3,21 @@ import os
 import os.path as path
 
 import pypiserver
+from passlib.apache import HtpasswdFile
 
 CWD = os.getcwd()
 PACKAGES = path.join(CWD, 'data')
 HTPASSWD = path.join(CWD, 'htpasswd')
 
-# USER_PASS = os.environ['PARAM1']
-#
-# if USER_PASS:
-#
-#     f = open(HTPASSWD, 'w')
-#
-#     f.writelines([ "%s\n" % line for line in USER_PASS.split(',')])
-#     f.close()
+USER_PASS = os.environ['PARAM1']
+if USER_PASS:
+    ht = HtpasswdFile(HTPASSWD)
+    for raw_user_password_pair in USER_PASS.split(','):
+        user, password = raw_user_password_pair.split(':', 2)
+        ht.set_password(user, password)
+
+    ht.save()
+
 
 try:
     os.makedirs(PACKAGES)
